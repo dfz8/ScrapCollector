5aedf03b23aa6a260bf3004d702e5f049eb2a2c2
Zhao, David <zhaod@pragmatics.com>
2016-07-06 17:00:35 -0400

    added basic DNA and Robot classes

---
+++ DNA.java
@@ -0,0 +1,39 @@
+public class DNA {
+	public static  enum actions {
+		UP, RIGHT, DOWN, LEFT, PICKUP, STAY;
+	};
+	private int[] genes;
+
+	public DNA(){
+		this.genes = new int[32];
+		for(int  i = 0; i < this.genes.length; i++) {
+			this.genes[i] = (int)(Math.random()*6);
+		}
+	}
+
+	public DNA(int[] genes){
+		this.genes = genes;
+	}
+
+	public int[] getGenes(){
+		return this.genes;
+	}
+
+	public actions getAction(int num){
+		switch(num){
+			case 0:
+				return actions.UP;
+			case 1:
+				return actions.RIGHT;
+			case 2:
+				return actions.DOWN;
+			case 3:
+				return actions.LEFT;
+			case 4:
+				return actions.PICKUP;
+			case 5:
+			default:
+				return actions.STAY;
+		}
+	}
+}
---
+++ Robot.java
@@ -0,0 +1,30 @@
+public class Robot {
+	private int energy;
+	private DNA dna;
+
+	public Robot(){
+		this.energy = 100;
+		this.dna = new DNA();
+	}
+
+	public Robot(DNA dna){
+		this.dna = dna;
+	}
+
+	public void update(int[] surroundings){
+		int index = 16*surroundings[0] +
+			8*surroundings[1] +
+			4*surroundings[2] +
+			2*surroundings[3] + 
+			surroundings[4];
+		switch(dna.getAction(index)){
+			case UP:
+			case LEFT:
+			case DOWN:
+			case RIGHT:
+			case PICKUP:
+			default:
+		}
+		this.energy--;
+	}
+}
